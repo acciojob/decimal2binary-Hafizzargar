3da4d788ff01616dc7a118745ea47fb360c6cefe
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2023-09-19 03:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function decimalToBinary(num) {
   //Write you code here
 	let s="";
 	while(num>0){
-		let d=num%2;
+		let d=Math.floor(num%2);
 		s=s+d;
-		num=num/10;
+		num=Math.floor(num/2);
 	}
 
 	return s;
